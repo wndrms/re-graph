0d8cff895d3afb3cd7e901ce9818e26c8b6d1ed3
wndrms <wndrms2006@naver.com>
2023-04-18 01:10:07 +0900

    Main 수정

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import driver from './neo4j'
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import './Main.css';
 
 const Main = () => {
 	const [addrlist, setaddrlist] = useState([]);
@@ -16,20 +17,20 @@ const Main = () => {
 			async function setaddresslist(driver) {
 				const session = driver.session({ database: 'neo4j' });
 				try {
-					const readQuery = 'Match (w:Wallet) RETURN DISTINCT w.hacker';
+					const readQuery = 'Match (w:Wallet) RETURN DISTINCT w.hacker, w.hackinfo';
 					const readResult = await session.executeRead(tx =>
 						tx.run(readQuery)
 					);
 					const adlist = [];
 					readResult.records.forEach(record => {
-						if (record.get('w.hacker') !== null)
-							adlist.push(record.get('w.hacker'));
+						if (record.get('w.hacker') !== null && record.get('w.hackinfo') !== null)
+							adlist.push({address: record.get('w.hacker'), info: record.get('w.hackinfo')});
 					})
 					setaddrlist(adlist);
 				} catch (error) {
 					console.error(`Something went wrong2: ${error}`);
 				} finally {
-					await session.close();
+					await driver.close();
 				}
 			}
 		})();
@@ -38,9 +39,22 @@ const Main = () => {
 		<div>
 			<h1>Hacker Wallet List</h1>
             <div style={{'display': 'flex', 'flexDirection':'column'}}>
-                {addrlist.map(a => {
-                    return <Link to={`/graph/${a}`} key={a}>{a}</Link>
-                })}
+				<table>
+					<thead>
+						<tr>
+							<th>Address</th>
+							<th>INFO</th>
+						</tr>
+					</thead>
+					<tbody>
+						{addrlist.map((addr) => (
+							<tr>
+								<td><Link to={`/graph/${addr.address}`}>{addr.address}</Link></td>
+								<td>{addr.info}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
             </div>
 		</div>
 	);
