2a1b4d50fb54b830f8819cbfeca7025baeddbda8
wndrms <wndrms2006@naver.com>
2023-05-16 14:28:12 +0900

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,62 +1,66 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import driver from './neo4j'
 
 const SearchBar = () => {
+	const [addrlist, setaddrlist] = useState([]);
     const [query, setQuery] = useState('');
+    const [matchAddr, setmatchAddr] = useState([]);
     const [error, seterror] = useState(false);
-    const [startDate, setStartDate] = useState();
-    const [startTime, setStartTime] = useState();
-    const [endDate, setEndDate] = useState();
-    const [endTime, setEndTime] = useState();
 
-    const instance = axios.create({
-        baseURL: 'http://localhost:8000/api/',
-        withCredentials: true,
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': 'csrftoken'
-        }
-    });
+    const navigate = useNavigate();
+
+    useEffect(() => {
+		(async () => {
+			try {
+				await setaddresslist(driver);
+			} catch (error) {
+				console.error(`Something went wrong1: ${error}`);
+			} 
+			async function setaddresslist(driver) {
+				const session = driver.session({ database: 'neo4j' });
+				try {
+					const readQuery = 'MATCH (n:EA) WHERE n.Address = n.Hacking_Case RETURN n.Hacking_Case';
+					const readResult = await session.executeRead(tx =>
+						tx.run(readQuery)
+					);
+					const adlist = [];
+					readResult.records.forEach(record => {
+						if (record.get('n.Hacking_Case') !== null)
+							adlist.push(record.get('n.Hacking_Case'));
+					})
+					setaddrlist(adlist);
+				} catch (error) {
+					console.error(`Something went wrong2: ${error}`);
+				} finally {
+					await session.close();
+				}
+			}
+		})();
+	}, [])
 
     function handleSubmit(event) {
         event.preventDefault();
-        instance.get('hello')
-            .then(response => console.log(response.data))
-            .catch(error => console.error(error));
-        const ethereumAddressRegex = /^0x[a-fA-F0-9]{40}$/;
-        if (!ethereumAddressRegex.test(query)) {
-            seterror(true);
-            return
-        }
-        seterror(false);
-        const startTimestamp = Date.parse(startDate + 'T' + startTime);
-        const endTimestamp = Date.parse(endDate + 'T' + endTime);
+        navigate('/graph/' + query);
 
     }
 
     function handleInputChange(event) {
         setQuery(event.target.value);
+        let tmpArray = [...addrlist];
+        tmpArray = tmpArray.filter((addr) => addr.includes(event.target.value));
+        if (event.target.value === '') tmpArray=[];
+        setmatchAddr(tmpArray);
     }
 
     return (
         <form onSubmit={handleSubmit} style={{'display': 'flex', 'flexDirection':'column'}}>
-            <label>
-                시작 날짜:
-                <input type="date" name="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
-            </label>
-            <label>
-                시작 시간:
-                <input type="time" name="startTime" value={startTime} onChange={(e) => setStartTime(e.target.value)}/>
-            </label>
-            <label>
-                종료 날짜:
-                <input type="date" name="endDate" value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
-            </label>
-            <label>
-                시작 시간:
-                <input type="time" name="endTime" value={endTime} onChange={(e) => setEndTime(e.target.value)}/>
-            </label>
             <input type="text" value={query} onChange={handleInputChange}/>
+            <ul>
+                {matchAddr.map((addr) => (
+                    <li onClick={() => setQuery(addr)}><button>{addr}</button></li>
+                ))}
+            </ul>
             {error && <p>정확한 주소를 입력해주세요</p>}
             <button type="submit">Search</button>
         </form>
